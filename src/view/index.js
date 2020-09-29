f1520a026394a6942d0e2c27cfbad8b7e6097a58
estefaniamoncada <estefaniamr716@gmail.com>
2020-09-29 16:43:08 -0500

    Primer commit: añadiendo vistas

---
+++ src/view/index.js
@@ -0,0 +1,11 @@
+import Sesion from './sesion.js';
+import Registro from './registro.js';
+import Different from './404.js';
+
+const components = {
+    sesion: Sesion,
+    registro: Registro,
+    different: Different
+}
+
+export { components };
