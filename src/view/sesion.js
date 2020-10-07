a97d27be8d4b9ea3391b7679e8456c713b45dcd4
katherin_moreno <morenodavilakatherin@gmail.com>
2020-10-06 23:43:23 -0500

    Estilos mobile y Desktop

--- src/view/sesion.js
+++ src/view/sesion.js
@@ -1,7 +1,9 @@
 export default () => {
   const viewSesion = `
     <div id="inicio-sesion">
-        <div class="cabecera"><img src="images/mochilerosqueda.svg"></div>
+        <div class="cabeceraMobile"><img src="images/mochilerosqueda.png"></div>
+        <div class="cabeceraDesktop"><img src="images/horizontal.png"></div>
+        <div class="galletita">
         <div class="logotipo"><img src="images/logo-RedSocial.png"></div>
         <h2 class="styleH2">¡Bienvenido mochilero!</h2>
         <div class="form">
@@ -12,26 +14,27 @@ export default () => {
         </div>
         <p class="textoSignIn">Iniciar sesión con Google o Facebook</p>
         <div class="signInRS">
-            <img src="images/faceb.png">
-            <img src="images/google.png">
+            <img src="images/googleRS.png">
         </div>
         <div class="registrate"><p>¿No tienes una cuenta?</p><a href="#/registro">Regístrate</a></div>
+        </div>
     </div>`;
 
-  const divElem = document.createElement('div');
+  const divElem = document.createElement("div");
   divElem.innerHTML = viewSesion;
-  const buttonSignIn = divElem.querySelector('#buttonSignIn');
-  buttonSignIn.addEventListener('click', () => {
-    const usuarioSignIn = divElem.querySelector('#usuarioSignIn').value;
-    const passwordSignIn = divElem.querySelector('#contraseñaSignIn').value;
+  const buttonSignIn = divElem.querySelector("#buttonSignIn");
+  buttonSignIn.addEventListener("click", () => {
+    const usuarioSignIn = divElem.querySelector("#usuarioSignIn").value;
+    const passwordSignIn = divElem.querySelector("#contraseñaSignIn").value;
     const auth = firebase.auth();
-    auth.signInWithEmailAndPassword(usuarioSignIn, passwordSignIn)
+    auth
+      .signInWithEmailAndPassword(usuarioSignIn, passwordSignIn)
       .then((userCredential) => {
-        console.log('Ha logrado iniciar sesión');
-        window.location.assign('#/timeline');
+        console.log("Ha logrado iniciar sesión");
+        window.location.assign("#/timeline");
       })
       .catch((error) => {
-        const errorContainer = divElem.querySelector('#errorMessage');
+        const errorContainer = divElem.querySelector("#errorMessage");
         const templateError = `<div class="modal-error"><p>Hubo un problema: ${error.message}</p></div>`;
         errorContainer.innerHTML = templateError;
       });
