f3b00128e2219f068c75b6907668084e994a690f
Veronica Silva <veronicasilvabarreto@gmail.com>
2020-10-09 21:31:39 -0500

    Cambio de ruta para importar auth js en firebasecontroller

--- src/firebase/firebasecontroller.js
+++ src/firebase/firebasecontroller.js
@@ -1,9 +1,9 @@
-import { logOut } from '../firebase/auth.js';
+import { logOut } from './auth.js';
 
 export const logOutEvent = () => {
-logOut()
-      .then(() => {
-        window.location.assign('#/');
-        console.log('¡Se cerró, lo logramos!');
-});
+  logOut()
+    .then(() => {
+      window.location.assign('#/');
+      console.log('¡Se cerró, lo logramos!');
+    });
 };
